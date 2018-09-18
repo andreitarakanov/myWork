c95a80c92547e2334e926c166ce802f90e30518a
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-09-18 08:04:35 -0500

    FavoriteMovie

---
+++ src/outputExamples/FavMovie.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class FavMovie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Fate of the furious");
+	}
+
+}
