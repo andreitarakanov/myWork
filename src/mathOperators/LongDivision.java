c824efd5120cfd730f82822c434523c5489ff86e
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-09-28 08:17:32 -0500

    Long Division

---
+++ src/mathOperators/LongDivision.java
@@ -0,0 +1,19 @@
+package mathOperators;
+
+import java.util.Scanner;
+
+public class LongDivision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("What do you want your dividend to be?");
+		double dividend = input.nextDouble();
+		System.out.println("What do you want your divisor to be?");
+		double divisor = input.nextDouble();
+		double remainder = dividend%divisor;
+		double whole = Math.round((dividend/divisor)-0.5);
+		System.out.println(whole+" r "+remainder);
+	}
+
+}
