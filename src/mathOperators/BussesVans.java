f8b6c82d88aae266cf017157b34536707220618f
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-09-28 08:56:53 -0500

    Busses Vans Improvements

--- src/mathOperators/BussesVans.java
+++ src/mathOperators/BussesVans.java
@@ -8,8 +8,9 @@ public class BussesVans {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		final long peoples = 35;
-		System.out.println("How many people are in your group?");
-		long people  = input.nextInt();
+		//System.out.println("How many people are in your group?");
+		//long people  = input.nextInt();
+		long people  = 37;
 		long bussesNeeded = Math.round((people/peoples)-0.5);
 		long peopleOver = people-peoples;
 		System.out.println("You have "+people+" in your group.");
