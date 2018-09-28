2c770384822230bc4b80cfd077f6c30a61ab64c9
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-09-28 08:03:30 -0500

    Circle Area Improvements

--- src/mathOperators/CircleArea.java
+++ src/mathOperators/CircleArea.java
@@ -6,11 +6,11 @@ public class CircleArea {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double pi = 3.1415;
+		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Whats the radius of your circle?");
 		double radius = input.nextDouble();
-		double area = (radius*radius)*pi;
+		double area = (radius*radius)*Math.PI;
 		System.out.println("The area of your circle is "+area);
 	}
 
