5b21ea5b3ad6ff54636e17c0d4c87a91e95971de
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-10-10 08:01:10 -0500

    Homework OOP
    10/10/18

---
+++ src/objectOriented/Car.java
@@ -0,0 +1,22 @@
+package objectOriented;
+
+public class Car {
+
+	public Car()
+	{
+		
+	}
+	public void sayOdometer()
+	{
+		System.out.println("6 miles driven on this car");
+	}
+	public void sayFlat()
+	{
+		System.out.println("The car has a flat tire!");
+	}
+	public void sayThirsty()
+	{
+		System.out.println("The car is out of gas");
+	}
+	
+}
---
+++ src/objectOriented/Neighbor.java
@@ -0,0 +1,20 @@
+package objectOriented;
+
+public class Neighbor {
+	public Neighbor()
+	{
+		
+	}
+	public void sayJoke()
+	{
+		System.out.println("Rats are brown");
+	}
+	public void sayMoney()
+	{
+		System.out.println("I need money");
+	}
+	public void sayConversation()
+	{
+		System.out.println("We need to talk");
+	}
+}
