e880ed8b4457a04519d60c1e2c39e00911d7d8e2
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-10-17 08:00:18 -0500

    Greeter OOP

--- src/objectOriented/Greeter.java
+++ src/objectOriented/Greeter.java
@@ -1,10 +1,19 @@
 package objectOriented;
 
 public class Greeter {
-
+	private int age;
 	public Greeter()
 	{
+		this.age = 14;
 		
+	}
+	public void setAge(int xAge)
+	{
+		age = xAge;
+	}
+	public int getAge()
+	{
+		return age;
 	}
 	public void sayHello()
 	{
