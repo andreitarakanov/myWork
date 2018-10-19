4da0951c4d5f5bf001fe639b4b125b4de66d798c
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-10-19 07:50:26 -0500

    Isosceles Triangle OOP

---
+++ src/objectOriented/IsoscelesTriangleTest.java
@@ -0,0 +1,12 @@
+package objectOriented;
+
+public class IsoscelesTriangleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IsoscelesTriangle bob = new IsoscelesTriangle(2,2,3,4);
+		System.out.println(bob.area());
+		System.out.println(bob.perimeter());
+	}
+
+}
