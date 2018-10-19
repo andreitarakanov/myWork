c0829c7628043588affc386d88d7f92ac4b501d9
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-10-19 07:50:49 -0500

    Right Triangle OOP

---
+++ src/objectOriented/RightTriangleTest.java
@@ -0,0 +1,13 @@
+package objectOriented;
+
+public class RightTriangleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RightTriangle bob = new RightTriangle(2,3);
+		System.out.println(bob.area());
+		System.out.println(bob.hypot());
+		System.out.println(bob.perimeter());
+	}
+
+}
