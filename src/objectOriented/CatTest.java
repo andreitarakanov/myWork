1d337f205ed240b7bc2c9a82b830873d938596c1
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-10-19 07:50:12 -0500

    Cat OOP

---
+++ src/objectOriented/CatTest.java
@@ -0,0 +1,15 @@
+package objectOriented;
+
+public class CatTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat crookshanks = new Cat();
+		crookshanks.sleepMore();
+		crookshanks.getSleep();
+		crookshanks.setAge(7);
+		crookshanks.birthday();
+		crookshanks.sleepLess();
+	}
+
+}
