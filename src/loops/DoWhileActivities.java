38c016ba341fe3a904fd0915d55183e045b67e40
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2018-11-20 08:15:03 -0600

    DoWhile Loop

--- src/loops/DoWhileActivities.java
+++ src/loops/DoWhileActivities.java
@@ -8,15 +8,26 @@ public class DoWhileActivities {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		int i = 0;
-		int si = 0;
-		int ix = 0;
+		int lower = 0;
+		int higher = 0;
 		do {
-			i = ix;
-			si = ix * 17;
-			System.out.println("Your magic number is " + si);
+			if (higher == 0 && lower == 0) {
+				higher = i;
+			} else if (lower == 0 && (i > higher || i == higher)) {
+				lower = higher;
+				higher = i;
+			} else if (i > higher) {
+				higher = i;
+			} else if (i < lower) {
+				lower = i;
+			} else if (lower == 0 && i < higher) {
+				lower = i;
+			}
 			System.out.println("Whats your number? 9999 to quit.");
-			ix = input.nextInt();
+			i = input.nextInt();
 		} while (i != 9999);
+		int difference = Math.abs(higher - lower);
+		System.out.println("Your difference is " + difference);
 	}
 
 }
