51f460e56ad8562911dfdd81ffaaae766027d663
Andrei Tarakanov <andrei.v.tarakanov@gmail.com>
2019-02-15 09:04:19 -0600

    didnt work

---
+++ src/machinelearning/testing.java
@@ -0,0 +1,14 @@
+package machinelearning;
+
+import sun.security.jca.GetInstance.Instance;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] values = new double[] { 1,2,3,4,5,6,7,8};
+		
+		Instance instance = new DenseInstance(values);
+	}
+
+}
